9ee8fe16d48420bcd7ca71b4f260b262b3ae05ec
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2016-06-13 00:13:04 +0200

    Add SqlFiber.Autofac project

---
+++ src/SqlFiber.Autofac/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SqlFiber.Autofac")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d36de4b6-f874-4fa3-8b8c-0573ca3ffc59")]
