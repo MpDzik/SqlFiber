28f703fcc3aa822bee4e9d3d57732064e8c9c69f
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2016-06-12 23:49:15 +0200

    Add unit tests project for SqlFiber.Core

---
+++ src/SqlFiber.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SqlFiber.Core.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0ba08c1e-9037-4dc3-814b-055eb936c8ad")]
